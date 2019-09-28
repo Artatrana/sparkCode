9c4c81be8bdd484201ad21d5ecfe2772be5860e5
Artatrana <artatrana.pujahari@gmail.com>
2019-09-27 21:12:14 -0700

    First Example Updated

--- sparkEltExample.scala
+++ sparkEltExample.scala
@@ -32,3 +32,39 @@ def exampleWriter()(df: DataFrame):Unit = {
   val path = "//Desktop/sparkCode/example"
   df.write.mode(SaveMode.OverWrite).parquet(path)
 }
+
+//EtlDefinition: Instantiate Etl Definition case class in spark-daria and use the process method to execute the ETL code
+
+val etl = new EtlDefinition(
+  soruceDF = extractDF,
+  transform = model(),
+  write = exampleWriter()
+)
+
+etl.prodess()
+
+
+//EtlDefinition method signature
+
+case Class EtlDefinition(
+  soruceDF: DataFrame,
+  transform: (DataFrame => DataFrame),
+  write: (DataFrame => Unit)
+  metadata: scala.collection.mutable.Map[String, Any]=
+scala.collection.mutable.Map[String, Any]()
+){
+  def process(): Unit = {
+    write(sourceDF.transform(transform))
+}
+
+// Multiple EtlDefinitions
+val etls = scala.collection.mutable.Map[string, EtlDefinition]()
+etls += ("bar" -> etl )
+etls += ("bar" -> etl2 )
+etls("bar").process()
+
+/*
+1> Make sure to repartition the DataFrame after filtering
+2> Custom DataFrame transformations should be broken up, tested individually, and then chained in a model() method
+3> Create EtlDefinition objects to organize your ETL logic and make sure all of your method signatures are correct
+*/
